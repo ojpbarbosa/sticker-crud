23eebff9ffda9665ce7dcc2778d413e2f65b292c
João Pedro Barbosa <joaopedrofbarbosa@protonmail.com>
2022-12-03 10:24:28 -0300

    chore: add attributes and methods to sticker header file

--- src/sticker.h
+++ src/sticker.h
@@ -1,19 +1,46 @@
-#ifndef Sticker
-#define Sricker
+#include <stdio.h>
+
+#ifndef STICKER
+#define STICKER
 
 class Sticker
 {
 private:
-  static char *player;
-  static char *team;
-  static char *field_position;
-  static char *birthday;
+  static const size_t REGISTER_SIZE =
+      sizeof(char) * 3 + sizeof(unsigned int) + sizeof(float) * 2;
 
-  static float weight;
-  static float height;
+  // char attributes
+  char *player_name;
+  char *birth_date;
+  char *national_team;
+
+  // int attributes
+  unsigned int *joined_year;
+
+  // float attributes
+  float player_weight;
+  float player_height;
 
 public:
-  static char *toString();
+  // constructors and destructors
+  Sticker();
+  Sticker(
+      char *player_name,
+      char *birth_date,
+      char *national_team,
+      unsigned int *joined_year,
+      float player_weight,
+      float player_height);
+  ~Sticker();
+
+  // see BinaryReader equivalent
+  Sticker *read_register(FILE *file);
+  // see BinaryWriter equivalent
+  void write_register(FILE *file);
+
+  // overrides
+  int compare(Sticker *sticker);
+  char *to_string();
 };
 
 #endif
